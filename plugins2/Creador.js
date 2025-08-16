2bbabb8e63478db5f0f82b4149c6ed3ee56fb2c6
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-08-16 03:00:16 -0400

    Actualizar Creador.js

--- plugins2/Creador.js
+++ plugins2/Creador.js
@@ -1,14 +1,12 @@
 const handler = async (msg, { conn }) => {
   const ownerNumber = "393715279301@s.whatsapp.net"; // Número del creador
-  const ownerName = "Russell xz 🤖"; // Nombre visible del creador
+  const ownerName = "ღܾ݉͢𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑ܾ݉ ִֶָ𓆪‌‹࣭݊𓂃ⷪ ִֶָ ᷫ‹ ⷭ.࣭𓆩‌۫᷼Ⴕ۫͜𓆪‌"; // Nombre visible del creador
 
-  const messageText = `📞 *Contacto del Creador del Subbot:*
+  const messageText = `Si tienes dudas, preguntas o sugerencias sobre el funcionamiento del *Subbot*, puedes contactar a su creador.
 
-Si tienes dudas, preguntas o sugerencias sobre el funcionamiento de *Azura Ultra Subbot*, puedes contactar a su creador.
-
-📌 *Nombre:*  ִֶָღܾ݉͢𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑ܾ݉ ִֶָ𓆪‌‹࣭݊𓂃ⷪ ִֶָ ᷫ‹ ⷭ.࣭𓆩‌۫᷼Ⴕ۫͜𓆪‌
-📌 *Número:* +393715279301
-💬 *Toca el contacto para enviarle un mensaje directo.`;
+❀ *Nombre:*  ִֶָღܾ݉͢𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑ܾ݉ ִֶָ𓆪‌‹࣭݊𓂃ⷪ ִֶָ ᷫ‹ ⷭ.࣭𓆩‌۫᷼Ⴕ۫͜𓆪‌
+❀ *Número:* +393715279301
+❀ *Toca el contacto para enviarle un mensaje directo.`;
 
   // Enviar contacto vCard
   await conn.sendMessage(msg.key.remoteJid, {
