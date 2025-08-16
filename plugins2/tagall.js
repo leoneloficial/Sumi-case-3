7e90374043d01b37712ee275497d7d713956f598
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-08-16 16:25:07 -0400

    Actualizar tagall.js

--- plugins2/tagall.js
+++ plugins2/tagall.js
@@ -37,10 +37,10 @@ const handler = async (msg, { conn, args }) => {
     }, { quoted: msg });
   }
 
-  const mentionList = participants.map(p => `❀ @${p.id.split("@")[0]}`).join("\n");
+  const mentionList = participants.map(p => `> ❀ @${p.id.split("@")[0]}`).join("\n");
   const extraMsg = args.join(" ");
-  let finalMsg = " *❀ INVOCACIÓN ❀*";
-  finalMsg += "٩(͡๏̯͡๏)۶ 🩷 ٩(͡๏̯͡๏)۶\n";
+  let finalMsg = " ❀ INVOCACIÓN ❀";
+  finalMsg += "\n٩(͡๏̯͡๏)۶ 🩷 ٩(͡๏̯͡๏)۶";
   if (extraMsg.trim().length > 0) {
     finalMsg += `\n❑ Mensaje: ${extraMsg}\n\n`;
   } else {
